fea0e93243b54581d4d11c5a25b426d8e9bb03ad
westsunshine <aisaxytsaeva@mail.ru>
2025-03-31 13:54:25 +0300

    refactoring user.py

--- src/routers/user.py
+++ src/routers/user.py
@@ -3,59 +3,78 @@ from models.user import User
 from db import get_database, Session
 from auth import get_user
 from schemas.user import UserMe, UserMeUpdate
-
+from typing import Optional
 import errors
 
+router = APIRouter(prefix="/users")
 
-router = APIRouter()
+class UserService:
+    @staticmethod
+    def get_user_me(user: User) -> UserMe:
+        return UserMe(
+            id=user.id,
+            name=user.user_info.name,
+            email=user.email,
+            username=user.username,
+            surname=user.user_info.surname,
+            patronymic=user.user_info.patronymic,  
+            phone=user.user_info.phone,
+            position=user.user_info.position,
+            joined_at=user.user_info.joined_at
+        )
+
+    @staticmethod
+    def update_user_info(
+        db: Session,
+        user: User,
+        update_data: UserMeUpdate
+    ) -> User:
+
+        if not user:
+            raise errors.unauthorized()
+
+        user_info = user.user_info
+        
+
+        field_mapping = {
+            'name': (user_info, 'name'),
+            'email': (user, 'email'),
+            'username': (user, 'username'),
+            'surname': (user_info, 'surname'),
+            'patronymic': (user_info, 'patronymic'),
+            'phone': (user_info, 'phone'),
+            'position': (user_info, 'position')
+        }
 
 
-@router.get('/me',
-            response_model=UserMe,
-            responses=errors.with_errors())
-async def get_me(user: User = Depends(get_user),
-                 db: Session = Depends(get_database)):
-    return UserMe(id=user.id,
-                  name=user.user_info.name,
-                  email=user.email,
-                  username=user.username,
-                  surname=user.user_info.surname,
-                  patronymic=user.user_info.name,
-                  phone=user.user_info.phone,
-                  position=user.user_info.position,
-                  joined_at=user.user_info.joined_at)
+        for field, (model, attr) in field_mapping.items():
+            if getattr(update_data, field) is not None:
+                setattr(model, attr, getattr(update_data, field))
 
+        db.commit()
+        return user
 
-@router.patch('/me',
-              responses=errors.with_errors())
-async def update_my_info(user_info: UserMeUpdate,
-                         user: User = Depends(get_user),
-                         db: Session = Depends(get_database)) -> UserMe:
-    if user is None:
-        raise errors.unauthorized()
+@router.get(
+    '/me',
+    response_model=UserMe,
+    responses=errors.with_errors()
+)
+async def get_current_user(
+    user: User = Depends(get_user),
+    db: Session = Depends(get_database)
+) -> UserMe:
+    """Get current user profile"""
+    return UserService.get_user_me(user)
 
-    if user_info.name is not None:
-        user.user_info.name = user_info.name
-    if user_info.email is not None:
-        user.email = user_info.email
-    if user_info.username is not None:
-        user.username = user_info.username
-    if user_info.surname is not None:
-        user.user_info.surname = user_info.surname
-    if user_info.patronymic is not None:
-        user.user_info.patronymic = user_info.patronymic
-    if user_info.phone is not None:
-        user.user_info.phone = user_info.phone
-    if user_info.position is not None:
-        user.user_info.position = user_info.position
-
-    db.commit()
-    return UserMe(id=user.id,
-                  name=user.user_info.name,
-                  email=user.email,
-                  username=user.username,
-                  surname=user.user_info.surname,
-                  patronymic=user.user_info.name,
-                  phone=user.user_info.phone,
-                  position=user.user_info.position,
-                  joined_at=user.user_info.joined_at)
+@router.patch(
+    '/me',
+    response_model=UserMe,
+    responses=errors.with_errors()
+)
+async def update_current_user(
+    update_data: UserMeUpdate,
+    user: User = Depends(get_user),
+    db: Session = Depends(get_database)
+) -> UserMe:
+    updated_user = UserService.update_user_info(db, user, update_data)
+    return UserService.get_user_me(updated_user)
